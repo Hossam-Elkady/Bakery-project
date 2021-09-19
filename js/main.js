d5e35ad3894636fa61d1a1d55779e47b08de0705
Hossam <hossamelkady722@gmail.com>
2021-09-19 08:19:55 +0200

    edited

--- js/main.js
+++ js/main.js
@@ -3,9 +3,18 @@ window.addEventListener('scroll', function () {
     let windowPosition = window.scrollY > 20;
     if (windowPosition == 0) {
         document.getElementById("navbar-brand").innerHTML = `<img src="images/bakery-light-1.png" alt="" class="my-auto">`
+        $(".bar1").css("background-color","white" )
+        $(".bar2").css("background-color","white" )
+        $(".bar3").css("background-color","white" )
     }
     else {
         document.getElementById("navbar-brand").innerHTML = `<img src="images/bakery-color.png" alt="" style="width: 11.25rem; height:3rem;"  class="my-auto">`
+        $(".bar1").css("background-color","black" )
+        $(".bar2").css("background-color","black" )
+        $(".bar3").css("background-color","black" )
     }
     nav.classList.toggle("scrolling-active", windowPosition)
 });
+function myFunction(x) {
+    x.classList.toggle("change");
+}
